e4d47a1efd4c5c5e850eede9e6c85ff81d056db2
Kelvin Mbewe <keokeil@hotmail.com>
2017-12-05 10:24:56 +0100

    selecting a video :relaxed::relaxed: :smile:

--- src/index.js
+++ src/index.js
@@ -16,10 +16,16 @@ import VideoDetail from './components/video_detail';
 class App extends Component {
   constructor (props){
     super(props);
-    this.state = {videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
     });
 
   }
@@ -27,8 +33,11 @@ class App extends Component {
   return (
     <div>
       <SearchBar />
-      <VideoDetail video={this.state.videos[0]}/>
-      <VideoList videos={this.state.videos} />
+      <VideoDetail video={this.state.selectedVideo}/>
+      <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+        videos={this.state.videos}
+      />
 
     </div>
   );
